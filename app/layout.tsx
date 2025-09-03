dc86ed2fb9afa78adca6e770c8bde9d1a6b0fd8a
Rafly Yusuf <107983288+rafly1012@users.noreply.github.com>
2025-09-03 15:55:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Rafly Yusuf | Full-Stack Developer Portfolio",
-  description: "Portfolio",
-  keywords: ["Rafly Yusuf", "Full-Stack Developer", "Portfolio"],
+  description:
+    "Rafly Yusuf is a Full-Stack Developer with 3+ years of experience building clean, user-friendly digital solutions. Passionate about continuous learning and innovation, he delivers work that makes a positive impact.",
+  keywords: ["Rafly Yusuf", "Full-Stack Developer"],
   authors: [{ name: "Rafly Yusuf" }],
   robots: {
     index: true,
@@ -40,8 +41,8 @@ export default function RootLayout({
               "@context": "https://schema.org",
               "@type": "Person",
               name: "Rafly Yusuf",
-              url: "https://raflyyusuf.com",
-              jobTitle: "Web Developer",
+              url: "https://rafly-yusuf.vercel.app",
+              jobTitle: "Full-Stack Developer",
               sameAs: [
                 "https://www.linkedin.com/in/rafly-yusuf-42b409177",
                 "https://github.com/rafly1012",
